474bb950b1bcc3862087203ca46097daf1feabc9
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2023-01-23 23:34:05 +0530

    prettier changes

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const mongoose = require('mongoose')
-const mongoURI = `mongodb+srv://ashutoshmishra:${process.env.MONGO_PASSWORD}@cluster0.9hv01oy.mongodb.net/?retryWrites=true&w=majority`
+const mongoose = require("mongoose");
+const mongoURI = `mongodb+srv://ashutoshmishra:${process.env.MONGO_PASSWORD}@cluster0.9hv01oy.mongodb.net/?retryWrites=true&w=majority`;
 
-const connectToMongo  = async () => {
-    mongoose.connect(mongoURI, () => {
-        console.log("Connected to Mongo successfully")
-    })
-}
+const connectToMongo = async () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to Mongo successfully");
+  });
+};
 
-module.exports = connectToMongo
+module.exports = connectToMongo;
