cfd78ad48c564059826aaa34d75b3dd00aa7ba64
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2023-01-23 23:34:24 +0530

    add meals auth

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json())
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/meals', require('./routes/meals'))
 
 app.listen(port, () => {
   console.log(`Meal tracker app listening on port ${port}`)
